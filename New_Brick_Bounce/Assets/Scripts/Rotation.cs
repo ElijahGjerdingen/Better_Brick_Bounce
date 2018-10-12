38c512852fbac0148056378280f7c4e5c37b4bbb
Samantha Ingersoll <samanthalingersoll@gmail.com>
2018-10-12 16:17:23 -0500

    Spinner Script

---
+++ New_Brick_Bounce/Assets/Scripts/Rotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour
+{
+    public float speed = 70f;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}

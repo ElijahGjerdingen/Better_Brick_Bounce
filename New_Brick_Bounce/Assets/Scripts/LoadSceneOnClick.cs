26953b06dc1a6ee57f2c003b0a134461a55e3b56
alexissaari <alexissaari@gmail.com>
2018-10-23 11:38:32 -0500

    Menu is #FullyFunctional
    Ya yeet.

--- New_Brick_Bounce/Assets/Scripts/LoadSceneOnClick.cs
+++ New_Brick_Bounce/Assets/Scripts/LoadSceneOnClick.cs
@@ -7,7 +7,7 @@ public class LoadSceneOnClick : MonoBehaviour {
 
     public void LoadByIndex(int sceneIndex)
     {
-        SceneManager.LoadScene(sceneIndex);
+        SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
     }
 
 }

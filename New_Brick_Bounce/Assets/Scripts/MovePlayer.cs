d0f7ba408f81b936d758dd8c673fb797d9d55f5f
Elijah Gjerdingen <elijah.gjerdingen@gmail.com>
2018-10-28 18:14:39 -0500

    I think I may have gotten the player moving!
    YAY!!

--- New_Brick_Bounce/Assets/Scripts/MovePlayer.cs
+++ New_Brick_Bounce/Assets/Scripts/MovePlayer.cs
@@ -4,17 +4,42 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour {
     public GameObject player;
-	// Use this for initialization
-	void Start () {
-        int position;
-        int direction;
-        int startpos;
-        int endpos;
+
+    int startPosition;
+    bool directionChosen;
+    Vector2 startPos;
+    Vector2 endingPos;
+    Vector2 direction;
+
+    // Use this for initialization
+    void Start () {
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        int position;
-        
+        if(Input.touches.Length > 0 && Input.touchCount == 1)
+        {
+            Touch current = Input.GetTouch(0);
+            Debug.Log(startPos);
+
+            switch(current.phase)
+            {
+                case TouchPhase.Began:
+                    startPos = current.position;
+                    directionChosen = false;
+                    break;
+                case TouchPhase.Moved:
+                    direction = current.position - startPos;
+                    break;
+                case TouchPhase.Ended:
+                    directionChosen = true;
+                    break;
+            }
+        }
+        if(directionChosen)
+        {
+            player.GetComponent<Rigidbody>().AddForce(direction);
+        }
 	}
 }

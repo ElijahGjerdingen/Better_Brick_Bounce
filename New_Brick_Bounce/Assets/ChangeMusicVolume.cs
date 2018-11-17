c8887979a5c5e6b9e5e6035b49c46b2a39236376
alexissaari <alexissaari@gmail.com>
2018-11-17 13:59:46 -0600

    Added Functional Audio Sliders with Background Music
    We added two sliders, one to change the background music and one to change the sound effects. Right now, both change the background music.

---
+++ New_Brick_Bounce/Assets/ChangeMusicVolume.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeMusicVolume : MonoBehaviour {
+
+    public Slider slider;
+
+    //public void Start()
+    //{
+    //    ChangeVolume(slider.value);
+    //}
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = slider.value;
+    }
+}
